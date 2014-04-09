e32aa9086a498132cb77e4c74284621e5132d295
Mikael Berg <miberg@silabs.com>
2014-04-09 11:21:10 +0200

    Renamed folders and output files from camelCase to dash

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
         },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
             jid: {
                 files: {
